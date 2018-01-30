d253562fe7b9dcf58117b7b10b5b928f4c0d9d65
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2018-01-30 10:00:52 -0500

    chance and terminal nodes

--- Intro.py
+++ Intro.py
@@ -1,23 +1,52 @@
 
 class ChanceNode:
-
-    def __init__(self, probs, costs):
+    def __init__(self, name, cost, future_nodes, probs):
+        self.name = name
+        self.cost = cost # cost of visiting this node
+        self.future_nodes = future_nodes  # list of future nodes
         self.probs = probs  # probabilities of outcomes
-        self.costs = costs  # costs of outcomes
 
     def get_expected_cost(self):
         """
         :return: the expected cost of this chance node
         """
-        num_outcomes = len(self.probs) # number of outcomes
-        exp_cost = 0 # expected cost initialized at 0
-        for i in range(num_outcomes):
-            exp_cost += self.probs[i] * self.costs[i]
-
+        exp_cost = self.cost  # expected cost initialized with the cost of visiting this node
+        i = 0  # index to iterate over probabilities
+        for thisNode in self.future_nodes:
+            if type(thisNode) == ChanceNode:
+                exp_cost += self.probs[i] * thisNode.get_expected_cost()
+            elif type(thisNode) == TerminalNode:
+                exp_cost += self.probs[i] * thisNode.get_cost()
+            i += 1
         return exp_cost
 
 
-# create an instance of ChanceNode
-myChanceNode = ChanceNode(probs=[0.1, 0.2, 0.7], costs=[10, 20, 30])
-# print the expect cost of this chance node
-print(myChanceNode.get_expected_cost())
+class TerminalNode:
+    def __init__(self, name, cost):
+        self.name = name
+        self.cost = cost  # cost of this terminal node
+
+    def get_cost(self):
+        """
+        :return: the cost of this terminal node
+        """
+        return self.cost
+
+
+# create the terminal nodes
+T1 = TerminalNode('T1', 10)
+T2 = TerminalNode('T2', 20)
+T3 = TerminalNode('T3', 30)
+T4 = TerminalNode('T4', 40)
+
+# create the future nodes of C2
+C2FutureNodes = [T1, T2, T3]
+# create C2
+C2 = ChanceNode('C2', 15, C2FutureNodes, [0.1, 0.2, 0.7])
+# create the future nodes of C1
+C1FutureNodes = [C2, T4]
+# create C1
+C1 = ChanceNode('C1', 0, C1FutureNodes, [0.5, 0.5])
+
+# print the expect cost of C1
+print(C1.get_expected_cost())
